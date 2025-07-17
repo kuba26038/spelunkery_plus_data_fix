67e6fae02b7605a52780538c82a33ed09bbdc145
TysonTheEmber <tysonpeterson2007@gmail.com>
2025-07-17 10:12:26 -0600

    Ore Gen Added

--- src/main/java/net/tysontheember/spelunkeryplus/worldgen/ModBiomeModifiers.java
+++ src/main/java/net/tysontheember/spelunkeryplus/worldgen/ModBiomeModifiers.java
@@ -1,4 +1,32 @@
 package net.tysontheember.spelunkeryplus.worldgen;
 
+import net.tysontheember.spelunkeryplus.SpelunkeryPlus;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraftforge.common.world.BiomeModifier;
+import net.minecraftforge.common.world.ForgeBiomeModifiers;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ModBiomeModifiers {
+    public static final ResourceKey<BiomeModifier> ADD_TUFF_SAPPHIRE_ORE = registerKey("add_tuff_sapphire_ore");
+
+
+    public static void bootstrap(BootstapContext<BiomeModifier> context) {
+        var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
+        var biomes = context.lookup(Registries.BIOME);//could try placed feature, should be able to find ice and fire's sapphire ore placed feature
+
+//        context.register(ADD_TUFF_SAPPHIRE_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+//                biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
+//                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.SAPPHIRE_ORE_PLACED_KEY)),
+//                GenerationStep.Decoration.UNDERGROUND_ORES));
+    }
+
+    private static ResourceKey<BiomeModifier> registerKey(String name) {
+        return ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(SpelunkeryPlus.MODID, name));
+    }
 }
